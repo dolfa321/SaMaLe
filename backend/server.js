709f4ef6a7109ad5ddce7709c1ba0f5f7555a6e4
Matic Lukežič <maticlukezic007@gmail.com>
2024-11-08 19:51:48 +0100

    Change mongo connection URL and test if it works

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import User from "./models/user.model.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -28,6 +29,26 @@ app.post("/api/users", (req, res) => {
   try {
     newUser.save();
     res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.type) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const newProduct = new Product({
+    name: product.name,
+    type: product.type,
+  });
+
+  try {
+    newProduct.save();
+    res.status(201).json(newProduct);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
