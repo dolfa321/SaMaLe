654c3ec11a5dc9d9e1c9779a50f50ca11b825b26
sabina <sabina.pauric@student.um.si>
2024-11-11 23:00:52 +0100

    Implemented home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 function App() {
   return (
       <div className="min-h-screen flex flex-col">
           <Header/>
           <div className="flex flex-col items-center justify-center flex-1">
-              <h1 className="text-3xl font-bold underline">App</h1>
               <Routes>
+                  <Route path="/" element={<Home/>}/>
                   <Route path="/login" element={<Login/>}/>
                   <Route path="/register" element={<Register/>}/>
               </Routes>
