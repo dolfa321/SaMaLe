7ef1dba19fcb815cd892092423c92f32487f1f64
sabina <sabina.pauric@student.um.si>
2024-11-11 23:31:32 +0100

    Added test to header when user is login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import logo from '../assets/logotip.png';
 import {Link} from "react-router-dom";
+
+
 const Header = () => {
+
+    const isLoggedIn = true;
+    const user = { firstName: "Janez", lastName: "Novak" };
+
+    const initials = user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase();
+
     return (
-        <header className="relative h-[130px] bg-white shadow-md flex items-center justify-center">
-            <Link to="/">
-                <img src={logo} alt="AI Logo" className="h-16 cursor-pointer" />
-            </Link>
+        <header className="relative h-[130px] bg-white shadow-md flex items-center justify-between px-8">
+            <div className="flex-1 flex justify-center">
+                <Link to="/">
+                    <img src={logo} alt="AI Logo" className="h-16 cursor-pointer"/>
+                </Link>
+            </div>
+
+            {isLoggedIn && (
+                <div className="absolute right-8">
+                    <div
+                        className="w-12 h-12 bg-[#EE6C4D] text-white rounded-full flex items-center justify-center text-lg font-bold">
+                        {initials}
+                    </div>
+                </div>
+            )}
         </header>
     );
 };
