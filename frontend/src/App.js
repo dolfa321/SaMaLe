a087770313e13908c31c84b31071fba12ad09a7d
sabina <sabina.pauric@student.um.si>
2024-11-11 22:13:29 +0100

    Added header components and implemented to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold underline ">App</h1>;
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
+      <div className="min-h-screen flex flex-col">
+          <Header/>
+          <div className="flex flex-col items-center justify-center flex-1">
+              <h1 className="text-3xl font-bold underline">App</h1>
+              <Routes>
+                  <Route path="/login" element={<Login/>}/>
+                  <Route path="/register" element={<Register/>}/>
+              </Routes>
+          </div>
+      </div>
   );
 }
 
