039f924eb49dcb40e01e6f23450cf89d8e920fde
sabina <sabina.pauric@student.um.si>
2024-11-12 20:44:10 +0100

    Added Route home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import UserContextProvider from "./context/userContext";
 import axios from "axios";
 import Profile from "./pages/Profile";
 import Chat from "./pages/Chat";
+import Home from "./pages/Home";
 
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.withCredentials = true;
@@ -17,9 +18,9 @@ function App() {
       <div className="min-h-screen flex flex-col">
         <Header />
         <div className="flex flex-col items-center justify-center flex-1">
-          <h1 className="text-3xl font-bold underline">App</h1>
           <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/profile" element={<Profile />} />
